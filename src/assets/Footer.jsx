ab06542b7c6bda4841d72a02ff0e6cca42e16c49
Jurre van Eijk <jurreveijk13@gmail.com>
2024-09-18 15:39:07 +0700

    finished version for now

--- src/assets/Footer.jsx
+++ src/assets/Footer.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import "../Footer.css";
 
 function Footer() {
 	return (
-		<footer>
-			<p>© 2024 Jurre van Eijk</p>
+		<footer id="footer">
+			<div class="footer-content">
+				<p>&copy; 2024 Jurre van Eijk. All Rights Reserved.</p>
+				<ul class="social-links">
+					<li>
+						<a href="mailto:jurreveijk13@gmail.com">Email</a>
+					</li>
+					<li>
+						<a
+							href="https://www.linkedin.com/in/jurre-van-eijk/"
+							target="_blank"
+						>
+							LinkedIn
+						</a>
+					</li>
+					<li>
+						<a href="https://github.com/JurrevE" target="_blank">
+							GitHub
+						</a>
+					</li>
+				</ul>
+				<p class="cta">
+					Let's work together!{" "}
+					<a href="mailto:jurreveijk13@gmail.com">Get in touch</a>
+				</p>
+			</div>
 		</footer>
 	);
 }
